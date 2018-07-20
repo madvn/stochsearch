a0e08c7d7fb5b408c58e7d7e100a94078a9d415d
Madhavun Candadai <madcanda@indiana.edu>
2018-07-20 17:31:45 -0400

    plot evol demo

--- demo/evolsearch_demo.py
+++ demo/evolsearch_demo.py
@@ -5,6 +5,7 @@ i.e. best solution is [1,1,1,1,1,1,1,1,1,1]
 '''
 
 import numpy as np
+import matplotlib.pyplot as plt
 from stochsearch import EvolSearch
 
 def fitness_function(individual):
@@ -17,31 +18,43 @@ def fitness_function(individual):
 evol_params = {
     'num_processes' : 4, # (optional) number of proccesses for multiprocessing.Pool
     'pop_size' : 100,    # population size
-    'genotype_size': 10, # dimensionality of solution
+    'genotype_size': 50, # dimensionality of solution
     'fitness_function': fitness_function, # custom function defined to evaluate fitness of a solution
     'elitist_fraction': 0.04, # fraction of population retained as is between generations
-    'mutation_variance': 0.05 # mutation noise added to offspring.
+    'mutation_variance': 0.2 # mutation noise added to offspring.
 }
 
 # create evolutionary search object
 es = EvolSearch(evol_params)
 
-'''OPTION 1'''
+'''OPTION 1
 # execute the search for 100 generations
 num_gens = 100
 es.execute_search(num_gens)
-
-'''OPTION 2
+'''
+'''OPTION 2'''
 # keep searching till a stopping condition is reached
+best_fit = []
+mean_fit = []
 num_gen = 0
 max_num_gens = 100
-desired_fitness = 0.75
-while es.get_best_individual_fitness() < desired_fitness and num_gen < max_num_gens:
+desired_fitness = 0.98
+#while es.get_best_individual_fitness() < desired_fitness and num_gen < max_num_gens:
+while num_gen < max_num_gens:
     print('Gen #'+str(num_gen)+' Best Fitness = '+str(es.get_best_individual_fitness()))
     es.step_generation()
+    best_fit.append(es.get_best_individual_fitness())
+    mean_fit.append(es.get_mean_fitness())
     num_gen += 1
-'''
 
 # print results
 print('Max fitness of population = ',es.get_best_individual_fitness())
 print('Best individual in population = ',es.get_best_individual())
+
+# plot results
+plt.figure()
+plt.plot(best_fit)
+plt.plot(mean_fit)
+plt.xlabel('Generations')
+plt.ylabel('Fitness')
+plt.show()
